6315e9efaf23fe2c10c3e6b3179daac3e72fff5b
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2023-06-27 23:13:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here
-
+let main = document.querySelector("#app");
+let btn = document.querySelector("#swap");
+function swapTheme(){
+	btn.addEventListener('click',function(e){
+		e.preventDefault();
+		main.className = "night"
+		btn.className = "button_night";
+	
+	})
+	
+}
+swapTheme();
